01d0818b61821d574944014f78fc0c4ba8fc9bd1
Hari Kumar <rsupriya2903@gmail.com>
2026-02-13 13:56:24 +0530

    Fix backend URL

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -20,7 +20,7 @@ const Pricing: React.FC = () => {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/create-order', {
+      const res = await fetch('fetch(`${import.meta.env.VITE_BACKEND_URL}/create-order`)', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ amount: price })
